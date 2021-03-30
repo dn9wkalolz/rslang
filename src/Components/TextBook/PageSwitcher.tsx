a33096fcb9d2de5ad8bb04d391f6947268d72a50
dn9wkalolz <64361765+dn9wkalolz@users.noreply.github.com>
2021-03-30 12:11:47 +0300

    Leo sprint and textbook PR #2 (#4)
    * feat: add base structure
    
    * feat: add styles, create base layout, add getting words feature and select right answer
    
    * feat: implement leosprint game mechanics
    
    * feat: added initial and final screen, game statistic, opportunity to choose difficulty
    
    * feat: added ability to listen audio at the of the game, add some style, fixed known bugs
    
    * feat: added textbook component, implemented showing words, page and group switcher
    
    * feat: added textbook styles, ability to switch page and group

--- src/Components/TextBook/PageSwitcher.tsx
+++ src/Components/TextBook/PageSwitcher.tsx
@@ -4,16 +4,38 @@ import { RootState } from '../../store/rootReducer';
 import { changePage } from '../../store/textbookActions';
 
 const PageSwitcher: React.FC = () => {
-  const page = useSelector((state: RootState) => state.textbookState.page);
+  const [page, group] = useSelector(
+    (state: RootState) => [state.textbookState.page, state.textbookState.group],
+  );
   const dispatch = useDispatch();
-  const switchPage = () => {
-    dispatch(changePage());
+  const switchPage = (e: React.MouseEvent) => {
+    const { name } = e.target as HTMLButtonElement;
+    const payload = name === 'decrement' ? -1 : 1;
+    dispatch(changePage(payload));
   };
+
   return (
-    <div>
-      <button type="button">Предыдущая страница</button>
-      <span>{page}</span>
-      <button type="button" onClick={switchPage}>Следующая страница</button>
+    <div className="textbook__page-switcher">
+      <button
+        name="decrement"
+        type="button"
+        onClick={switchPage}
+        disabled={page === 0}
+      >
+        Предыдущая страница
+      </button>
+      <div className="textbook__status">
+        <h1>{`Группа слов №${group + 1}`}</h1>
+        <h2>{`Страница №${page + 1}`}</h2>
+      </div>
+      <button
+        name="increment"
+        type="button"
+        onClick={switchPage}
+        disabled={page === 29}
+      >
+        Следующая страница
+      </button>
     </div>
   );
 };
--- src/data/content.ts
+++ src/data/content.ts
@@ -206,3 +206,7 @@ export const ownGameContent = {
   learned: 'Изучено:',
   tolearn: 'Для повторения',
 };
+
+export const textBookContent = {
+  groups: ['Раздел 1', 'Раздел 2', 'Раздел 3', 'Раздел 4', 'Раздел 5', 'Раздел 6'],
+};
