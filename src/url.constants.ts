5ce1d89b80d1635a33349c5d9e448acac70d8d74
Alexei <demidenko8888@gmail.com>
2021-04-05 16:47:29 +0300

    fix: change correct API_URL

--- src/url.constants.ts
+++ src/url.constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://learnlangs.herokuapp.com/';
+export const API_URL = 'https://rslang-61.herokuapp.com/';
 
 export const API_URL_ENDPOINTS = {
   signin: `${API_URL}/signin`,
