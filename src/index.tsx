a5f006c030b3a1eca08cf11daf0bbfc33e0f6b77
sergey <dn9wkalolz@gmail.com>
2021-04-14 20:41:44 +0300

    refactor: api url

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { compose, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import App from './App';
@@ -9,11 +9,9 @@ import './assets/css/reset.css';
 import './assets/css/base/fontello.css';
 import './assets/css/base/common.scss';
 
-// @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk)),
+  applyMiddleware(thunk),
 );
 
 const app = (
--- src/url.constants.ts
+++ src/url.constants.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'https://rslang-61.herokuapp.com/';
-export const API_CLOUDINARY_URL = 'http://api.cloudinary.com/v1_1/doxp7mppe/image/';
+export const API_CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/doxp7mppe/image/';
 
 export const API_URL_ENDPOINTS = {
   signin: `${API_URL}/signin`,
